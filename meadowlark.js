c5ed8e3d90e4f67b1105b427dce80a5143eaefa8
Dima Mihnevich <dima.mihnevich@gmail.com>
2022-02-25 10:50:44 +0300

    last fix

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const expressHandlebars = require('express-handlebars');
-const fortune = require('./lib/fortune')
+const expressHandlebars = require('express-handlebars')
+const handlers = require('./lib/handlers')
 const app = express()
 
 const hbs = expressHandlebars.create({
-	defaultLayout: 'main',
-	extname: 'hbs'/* ,
-	runtimeOptions: {
-		allowProtoPropertiesByDefault: true,
-		allowProtoMethodsByDefault: true
-	} */
+    defaultLayout: 'main',
+    extname: 'hbs'/* ,
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true
+  } */
 })
 app.engine('hbs', hbs.engine)
 app.set('views', 'views')
@@ -17,21 +17,18 @@ app.set('view engine', 'hbs')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) => res.render('about', {fortune: fortune.getFortune()}))
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`))
+if (require.main === module) {
+    app.listen(port, () => console.log(
+        `Express started on http://localhost:${port}; ` +
+        `press Ctrl-C to terminate.`))
+} else {
+    module.exports = app
+}
