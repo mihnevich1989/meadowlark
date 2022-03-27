75954aedbf8a7a59748df365563ebe272ea9466e
Dima Mihnevich <d.mi@nuwber.com>
2022-03-27 12:29:06 +0300

    form

--- meadowlark.js
+++ meadowlark.js
@@ -1,18 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const expressHandlebars = require('express-handlebars')
 const handlers = require('./lib/handlers')
 const app = express()
 
 const hbs = expressHandlebars.create({
-    defaultLayout: 'main',
+  defaultLayout: 'main',
   extname: 'hbs',
-    helpers: {
-      section: function (name, options) {
-        if (!this._sections) this._sections = {}
-        this._sections[name] = options.fn(this)
-        return null
-      }
-    }  
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    }
+  }
   /* ,
   runtimeOptions: {
     allowProtoPropertiesByDefault: true,
@@ -24,9 +25,15 @@ app.set('views', 'views')
 app.set('view engine', 'hbs')
 
 app.use(express.static(__dirname + '/public'))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', handlers.home)
 app.get('/about', handlers.about)
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.get('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
 // custom 404 page
 app.use(handlers.notFound)
 // custom 500 page
@@ -34,9 +41,9 @@ app.use(handlers.serverError)
 
 const port = process.env.PORT || 3000
 if (require.main === module) {
-    app.listen(port, () => console.log(
-        `Express started on http://localhost:${port}; ` +
-        `press Ctrl-C to terminate.`))
+  app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`))
 } else {
-    module.exports = app
+  module.exports = app
 }
