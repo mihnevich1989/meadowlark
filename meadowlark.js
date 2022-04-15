b08af2b537332f5d7bbbec581b91cdebdf0d0822
Dima Mihnevich <d.mi@nuwber.com>
2022-04-15 15:12:03 +0300

    form partials

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const expressHandlebars = require('express-handlebars')
+const multiparty = require('multiparty')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
 const handlers = require('./lib/handlers')
+const { credentials } = require('./config')
 const app = express()
 
 const hbs = expressHandlebars.create({
@@ -23,9 +27,16 @@ const hbs = expressHandlebars.create({
 app.engine('hbs', hbs.engine)
 app.set('views', 'views')
 app.set('view engine', 'hbs')
+app.use(cookieParser(credentials.cookieSecret))
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: credentials.cookieSecret
+}))
 
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.get('/', handlers.home)
 app.get('/about', handlers.about)
 app.get('/newsletter', handlers.newsletter)
@@ -34,6 +45,25 @@ app.get('/newsletter-signup', handlers.newsletterSignup)
 app.get('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if (err) return handlers.vacationPhotoContestProcessError(req, res, err.message)
+    console.log('got fields: ', fields)
+    console.log('and files: ', files)
+    handlers.vacationPhotoContestProcess(req, res, fields, files)
+  })
+})
+app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou)
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if (err) return handlers.api.vacationPhotoContestError(req, res, err.message)
+    handlers.api.vacationPhotoContest(req, res, fields, files)
+  })
+})
 // custom 404 page
 app.use(handlers.notFound)
 // custom 500 page
